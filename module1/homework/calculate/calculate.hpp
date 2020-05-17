9cf63b41173145d12d0eb5f4695ef68a875e2198
Rafal <r.massarnowski@gmail.com>
2020-05-17 15:10:10 +0200

    add calculate function

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,28 @@
 #pragma once
+#include <functional>
+#include <map>
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+std::string calculate(const std::string &command, int first, int second) {
+  auto calculator =
+      std::map<std::string, std::function<std::string(int, int)>>{};
+
+  calculator.try_emplace("add", [&](const auto first, const auto second) {
+    return std::to_string(first + second);
+  });
+  calculator.try_emplace("subtract", [&](const auto first, const auto second) {
+    return std::to_string(first - second);
+  });
+  calculator.try_emplace("multiply", [&](const auto first, const auto second) {
+    return std::to_string(first * second);
+  });
+  calculator.try_emplace("divide", [&](const auto first, const auto second) {
+    return second != 0 ? std::to_string(first / second) : "Division by 0";
+  });
+
+  if (calculator.find(command) != calculator.end()) {
+    return calculator[command](first, second);
+  }
+
+  return "Invalid data";
 }
