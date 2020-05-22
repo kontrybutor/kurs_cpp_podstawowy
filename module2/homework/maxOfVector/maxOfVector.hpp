2bad9ab17ae85d25e6e8bbacec2e9e806ad2cf84
Kuba C <jakub.cieplinski@outlook.com>
2020-05-23 00:15:40 +0200

    maxOfVector implemented

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -4,5 +4,10 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int maxNumber = vec[0];
+    for(const auto& number: vec){
+        maxNumber = number > maxNumber ? number : maxNumber;
+    }
+
+    return maxNumber;
 }
