bcd5ca6f802ab3af3eea5a800afbfda4c60c6fbb
Rafal <r.massarnowski@gmail.com>
2020-05-25 11:46:29 +0200

    NWD and NWW implemented

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,16 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+  if (lhs == 0) {
+    return abs(rhs);
+  } else if (rhs == 0) {
+    return abs(lhs);
+  }
+  return NWD(rhs, lhs % rhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+  return (lhs != 0 && rhs != 0) ? (abs(lhs) / NWD(lhs, rhs)) * abs(rhs) : 0;
 }
