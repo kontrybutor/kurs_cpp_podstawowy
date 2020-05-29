d95be629f072b415a47a05d37f47ffd3dbe12ff4
Rafal <r.massarnowski@gmail.com>
2020-05-29 21:39:59 +0200

    add basic implementation of doesPasswordMatch and checkPassword funtions

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -3,34 +3,49 @@
 #include <map>
 using EC = ErrorCode;
 
-std::string getErrorMessage(const ErrorCode &err) {
-
-    const std::map<ErrorCode, std::string> errorMap = {
-        {EC::Ok, "OK"},
+std::string getErrorMessage(const ErrorCode &err)
+{
+    std::string result;
+    std::map<ErrorCode, std::string> errorMap = {
+        {EC::Ok, "YourPasswordIsOK"},
         {EC::PasswordNeedsAtLeastNineCharacters, "NeedsAtLeastNineCharacters"},
         {EC::PasswordNeedsAtLeastOneNumber, "NeedsAtLeastOneNumber"},
         {EC::PasswordNeedsAtLeastOneSpecialCharacter, "NeedsOneSpecialCharacter"},
         {EC::PasswordNeedsAtLeastOneUppercaseLetter, "NeedsAtLeastOneUppercaseLetter"},
-        {EC::PasswordsDoesNotMatch, "PasswordsDoesNotMatch"}
-        };
+        {EC::PasswordsDoesNotMatch, "PasswordsDoesNotMatch"}};
 
-    return "";
+    if (errorMap.contains(err))
+    {
+        result = errorMap[err];
+    }
+    return result;
 }
 
 bool doesPasswordsMatch(const std::string &passwd1,
-                        const std::string &passwd2) {
-    return false;
+                        const std::string &passwd2)
+{
+    bool result = false;
+    if (passwd1 == passwd2)
+    {
+        result = true;
+    }
+    return result;
 }
 
-ErrorCode checkPasswordRules(const std::string &passwd) {
+ErrorCode checkPasswordRules(const std::string &passwd)
+{
     ErrorCode err;
 
     return err;
 }
 
 ErrorCode checkPassword(const std::string &passwd1,
-                        const std::string &passwd2) {
-    ErrorCode err;
+                        const std::string &passwd2)
+{
 
-    return err;
+    if (doesPasswordsMatch(passwd1, passwd2))
+    {
+        return ErrorCode::Ok;
+    }
+    return ErrorCode::PasswordsDoesNotMatch;
 }
