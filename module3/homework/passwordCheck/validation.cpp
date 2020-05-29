4b61c98d9c89c3061c64e5847386814da71b4635
Rafal <r.massarnowski@gmail.com>
2020-05-29 21:05:31 +0200

    map filled

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,24 +1,35 @@
 #include "validation.hpp"
 
-std::string getErrorMessage(const ErrorCode &err)
-{
+#include <map>
+using EC = ErrorCode;
+
+std::string getErrorMessage(const ErrorCode &err) {
+
+    const std::map<ErrorCode, std::string> errorMap = {
+        {EC::Ok, "OK"},
+        {EC::PasswordNeedsAtLeastNineCharacters, "NeedsAtLeastNineCharacters"},
+        {EC::PasswordNeedsAtLeastOneNumber, "NeedsAtLeastOneNumber"},
+        {EC::PasswordNeedsAtLeastOneSpecialCharacter, "NeedsOneSpecialCharacter"},
+        {EC::PasswordNeedsAtLeastOneUppercaseLetter, "NeedsAtLeastOneUppercaseLetter"},
+        {EC::PasswordsDoesNotMatch, "PasswordsDoesNotMatch"}
+        };
+
     return "";
 }
 
-bool doesPasswordsMatch(const std::string & passwd1, const std::string & passwd2)
-{
+bool doesPasswordsMatch(const std::string &passwd1,
+                        const std::string &passwd2) {
     return false;
 }
 
-ErrorCode checkPasswordRules(const std::string & passwd)
-{
+ErrorCode checkPasswordRules(const std::string &passwd) {
     ErrorCode err;
 
     return err;
 }
 
-ErrorCode checkPassword(const std::string & passwd1, const std::string & passwd2)
-{
+ErrorCode checkPassword(const std::string &passwd1,
+                        const std::string &passwd2) {
     ErrorCode err;
 
     return err;
